1ae7c228144937fe33f9e0d3ba6754ca77325f6c
Devansh Tiwari <devanshtiwari817@gmail.com>
2026-06-02 09:34:49 +0530

    Update device.js

--- PORTFOLIO/src/utils/device.js
+++ PORTFOLIO/src/utils/device.js
@@ -2,3 +2,10 @@
 export const isTouchDevice =
   typeof window !== "undefined" &&
   window.matchMedia("(hover: none)").matches;
+
+export function getMailHref() {
+  if (typeof navigator === "undefined") return "mailto:devanshtiwari817@gmail.com";
+  return /Android|iPhone/i.test(navigator.userAgent)
+    ? "mailto:devanshtiwari817@gmail.com"
+    : "https://mail.google.com/mail/?view=cm&fs=1&to=devanshtiwari817@gmail.com";
+}
